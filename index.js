694c3f6986e7a4a4c2e3af1cfd74bdf200e31853
Simpson <jonsimpson@LM-sjc-00710022.(none)>
2012-11-15 02:28:07 -0800

    pre demo

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var TCProxy = require('./tcp');
+var TCProxy = require('./lib/tcp.proxy');
 
-var HTTProxy = require('./http');
+var HTTProxy = require('./lib/proxy');
+
+var monitor  = require('./monitor')
 
 module.exports.tcp = TCProxy
 
 module.exports.http = HTTProxy
+
+module.exports.monitor = monitor
