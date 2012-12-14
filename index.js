4f889ceccec053801b26e6f187a5ab807fe22c8b
Simpson <jonsimpson@LM-sjc-00710022.(none)>
2012-12-14 15:09:08 -0800

    the very latest

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var HTTProxy = require('./lib/proxy');
 
 var monitor  = require('./monitor');
 
-var configuration_server = require('./lib/config.server');
+//var configuration_server = require('./lib/config.server');
 
 module.exports.tcp = TCProxy
 
--- public/bundle.js
+++ public/bundle.js
@@ -391,573 +391,93 @@ process.binding = function (name) {
 
 });
 
-require.define("/configs/sample.config.json",function(require,module,exports,__dirname,__filename,process,global){module.exports = {
-	"global" : {
-		"routes" : {
-			"/{x}?/{y}":{
-				"host":"127.0.0.1",
-		        "port":3200,
-		        "url":"/test.json",
-		        "type":"apples",
-				"method":"GET",
-		        "latency":3330,
-				"array" : ["127.0.0.1", "0.0.0.0", {boogie: "woogie"}, [1000, 2000, 4000]]
-			},
-			"/default":{
-				"host":"127.0.0.1",
-		        "port":3200,
-		        "url":"/test.json",
-		        "type":"bananas",
-				"method":"POST"
-			}
-		},
-		"IP ADDRESSES" : ["127.0.0.1", "0.0.0.0", {boogie: "woogie"}, [1000, 2000, 4000]]
-	},
-	
-	"tenant 1" : {
-		"routes" : {
-			"/{z}?/{w}":{
-				"host":"127.0.0.1",
-		        "port":3200,
-		        "url":"/test.json",
-		        "type":"apples",
-				"method":"GET",
-		        "latency":3330
-			},
-			"/standard":{
-				"host":"127.0.0.1",
-		        "port":3200,
-		        "url":"/test.json",
-		        "type":"bananas",
-				"method":"POST"
-			}
-		},
-		"IP ADDRESSES" : ["127.0.0.1", "0.0.0.0	"]
-	}
-}
-;
-
+require.define("/node_modules/domready/package.json",function(require,module,exports,__dirname,__filename,process,global){module.exports = {"main":"./ready.js"}
 });
 
-require.define("/lib/mouse.around.js",function(require,module,exports,__dirname,__filename,process,global){module.exports = function(node, fn){
-	
-	node.addEventListener('mouseover', onHover, true)
-	node.addEventListener('mouseout', onExit, true)
-	
-	var node = node;
-	var position = [0, 0];
-	
-	function mouseMove(evt){
-		fn(evt, node, position, false, false)
-	}
-	
-	function onExit(evt){
-		window.removeEventListener('mousemove', mouseMove, true)
-		fn(evt, node, position, false, true)
-	}
-	
-	function onHover(evt){
-		window.addEventListener('mousemove', mouseMove, true)
-		position = findPos(evt.target);
+require.define("/node_modules/domready/ready.js",function(require,module,exports,__dirname,__filename,process,global){/*!
+  * domready (c) Dustin Diaz 2012 - License MIT
+  */
+!function (name, definition) {
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+}('domready', function (ready) {
 
-		fn(evt, node, position, true, false)
+  var fns = [], fn, f = false
+    , doc = document
+    , testEl = doc.documentElement
+    , hack = testEl.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , addEventListener = 'addEventListener'
+    , onreadystatechange = 'onreadystatechange'
+    , readyState = 'readyState'
+    , loaded = /^loade|c/.test(doc[readyState])
 
-		function findPos(obj) {
-				var curleft = curtop = 0;
-				if (obj.offsetParent) {
-					do {
-								curleft += obj.offsetLeft;
-								curtop += obj.offsetTop;
-							} 
-					while (obj = obj.offsetParent);
-					return [curleft,curtop];
-				};
-			};
+  function flush(f) {
+    loaded = 1
+    while (f = fns.shift()) f()
+  }
+
+  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
+    doc.removeEventListener(domContentLoaded, fn, f)
+    flush()
+  }, f)
+
+
+  hack && doc.attachEvent(onreadystatechange, fn = function () {
+    if (/^c/.test(doc[readyState])) {
+      doc.detachEvent(onreadystatechange, fn)
+      flush()
+    }
+  })
+
+  return (ready = hack ?
+    function (fn) {
+      self != top ?
+        loaded ? fn() : fns.push(fn) :
+        function () {
+          try {
+            testEl.doScroll('left')
+          } catch (e) {
+            return setTimeout(function() { ready(fn) }, 50)
+          }
+          fn()
+        }()
+    } :
+    function (fn) {
+      loaded ? fn() : fns.push(fn)
+    })
+})
+});
+
+require.define("/lib/bundle.js",function(require,module,exports,__dirname,__filename,process,global){var domready = require('domready')
+  , xhr = new XMLHttpRequest()
+  ;
+
+domready(function(){
+	// create the editor
+    var container = document.getElementById("jsoneditor");
+    var save = document.getElementById("save");
+
+    var editor = new JSONEditor(container);
+    xhr.open('GET', '../getConfig', true);
+    xhr.onload = function(e){
+		editor.set(JSON.parse(xhr.responseText));
 	};
-	
-}
-});
-
-require.define("/node_modules/traverse/package.json",function(require,module,exports,__dirname,__filename,process,global){module.exports = {"main":"index.js"}
-});
-
-require.define("/node_modules/traverse/index.js",function(require,module,exports,__dirname,__filename,process,global){var traverse = module.exports = function (obj) {
-    return new Traverse(obj);
-};
-
-function Traverse (obj) {
-    this.value = obj;
-}
-
-Traverse.prototype.get = function (ps) {
-    var node = this.value;
-    for (var i = 0; i < ps.length; i ++) {
-        var key = ps[i];
-        if (!Object.hasOwnProperty.call(node, key)) {
-            node = undefined;
-            break;
-        }
-        node = node[key];
-    }
-    return node;
-};
-
-Traverse.prototype.has = function (ps) {
-    var node = this.value;
-    for (var i = 0; i < ps.length; i ++) {
-        var key = ps[i];
-        if (!Object.hasOwnProperty.call(node, key)) {
-            return false;
-        }
-        node = node[key];
-    }
-    return true;
-};
-
-Traverse.prototype.set = function (ps, value) {
-    var node = this.value;
-    for (var i = 0; i < ps.length - 1; i ++) {
-        var key = ps[i];
-        if (!Object.hasOwnProperty.call(node, key)) node[key] = {};
-        node = node[key];
-    }
-    node[ps[i]] = value;
-    return value;
-};
-
-Traverse.prototype.map = function (cb) {
-    return walk(this.value, cb, true);
-};
-
-Traverse.prototype.forEach = function (cb) {
-    this.value = walk(this.value, cb, false);
-    return this.value;
-};
-
-Traverse.prototype.reduce = function (cb, init) {
-    var skip = arguments.length === 1;
-    var acc = skip ? this.value : init;
-    this.forEach(function (x) {
-        if (!this.isRoot || !skip) {
-            acc = cb.call(this, acc, x);
-        }
-    });
-    return acc;
-};
-
-Traverse.prototype.paths = function () {
-    var acc = [];
-    this.forEach(function (x) {
-        acc.push(this.path); 
-    });
-    return acc;
-};
-
-Traverse.prototype.nodes = function () {
-    var acc = [];
-    this.forEach(function (x) {
-        acc.push(this.node);
-    });
-    return acc;
-};
-
-Traverse.prototype.clone = function () {
-    var parents = [], nodes = [];
-    
-    return (function clone (src) {
-        for (var i = 0; i < parents.length; i++) {
-            if (parents[i] === src) {
-                return nodes[i];
-            }
-        }
-        
-        if (typeof src === 'object' && src !== null) {
-            var dst = copy(src);
-            
-            parents.push(src);
-            nodes.push(dst);
-            
-            forEach(objectKeys(src), function (key) {
-                dst[key] = clone(src[key]);
-            });
-            
-            parents.pop();
-            nodes.pop();
-            return dst;
-        }
-        else {
-            return src;
-        }
-    })(this.value);
-};
-
-function walk (root, cb, immutable) {
-    var path = [];
-    var parents = [];
-    var alive = true;
-    
-    return (function walker (node_) {
-        var node = immutable ? copy(node_) : node_;
-        var modifiers = {};
-        
-        var keepGoing = true;
-        
-        var state = {
-            node : node,
-            node_ : node_,
-            path : [].concat(path),
-            parent : parents[parents.length - 1],
-            parents : parents,
-            key : path.slice(-1)[0],
-            isRoot : path.length === 0,
-            level : path.length,
-            circular : null,
-            update : function (x, stopHere) {
-                if (!state.isRoot) {
-                    state.parent.node[state.key] = x;
-                }
-                state.node = x;
-                if (stopHere) keepGoing = false;
-            },
-            'delete' : function (stopHere) {
-                delete state.parent.node[state.key];
-                if (stopHere) keepGoing = false;
-            },
-            remove : function (stopHere) {
-                if (isArray(state.parent.node)) {
-                    state.parent.node.splice(state.key, 1);
-                }
-                else {
-                    delete state.parent.node[state.key];
-                }
-                if (stopHere) keepGoing = false;
-            },
-            keys : null,
-            before : function (f) { modifiers.before = f },
-            after : function (f) { modifiers.after = f },
-            pre : function (f) { modifiers.pre = f },
-            post : function (f) { modifiers.post = f },
-            stop : function () { alive = false },
-            block : function () { keepGoing = false }
-        };
-        
-        if (!alive) return state;
-        
-        function updateState() {
-            if (typeof state.node === 'object' && state.node !== null) {
-                if (!state.keys || state.node_ !== state.node) {
-                    state.keys = objectKeys(state.node)
-                }
-                
-                state.isLeaf = state.keys.length == 0;
-                
-                for (var i = 0; i < parents.length; i++) {
-                    if (parents[i].node_ === node_) {
-                        state.circular = parents[i];
-                        break;
-                    }
-                }
-            }
-            else {
-                state.isLeaf = true;
-                state.keys = null;
-            }
-            
-            state.notLeaf = !state.isLeaf;
-            state.notRoot = !state.isRoot;
-        }
-        
-        updateState();
-        
-        // use return values to update if defined
-        var ret = cb.call(state, state.node);
-        if (ret !== undefined && state.update) state.update(ret);
-        
-        if (modifiers.before) modifiers.before.call(state, state.node);
-        
-        if (!keepGoing) return state;
-        
-        if (typeof state.node == 'object'
-        && state.node !== null && !state.circular) {
-            parents.push(state);
-            
-            updateState();
-            
-            forEach(state.keys, function (key, i) {
-                path.push(key);
-                
-                if (modifiers.pre) modifiers.pre.call(state, state.node[key], key);
-                
-                var child = walker(state.node[key]);
-                if (immutable && Object.hasOwnProperty.call(state.node, key)) {
-                    state.node[key] = child.node;
-                }
-                
-                child.isLast = i == state.keys.length - 1;
-                child.isFirst = i == 0;
-                
-                if (modifiers.post) modifiers.post.call(state, child);
-                
-                path.pop();
-            });
-            parents.pop();
-        }
-        
-        if (modifiers.after) modifiers.after.call(state, state.node);
-        
-        return state;
-    })(root).node;
-}
-
-function copy (src) {
-    if (typeof src === 'object' && src !== null) {
-        var dst;
-        
-        if (isArray(src)) {
-            dst = [];
-        }
-        else if (isDate(src)) {
-            dst = new Date(src);
-        }
-        else if (isRegExp(src)) {
-            dst = new RegExp(src);
-        }
-        else if (isError(src)) {
-            dst = { message: src.message };
-        }
-        else if (isBoolean(src)) {
-            dst = new Boolean(src);
-        }
-        else if (isNumber(src)) {
-            dst = new Number(src);
-        }
-        else if (isString(src)) {
-            dst = new String(src);
-        }
-        else if (Object.create && Object.getPrototypeOf) {
-            dst = Object.create(Object.getPrototypeOf(src));
-        }
-        else if (src.constructor === Object) {
-            dst = {};
-        }
-        else {
-            var proto =
-                (src.constructor && src.constructor.prototype)
-                || src.__proto__
-                || {}
-            ;
-            var T = function () {};
-            T.prototype = proto;
-            dst = new T;
-        }
-        
-        forEach(objectKeys(src), function (key) {
-            dst[key] = src[key];
-        });
-        return dst;
-    }
-    else return src;
-}
-
-var objectKeys = Object.keys || function keys (obj) {
-    var res = [];
-    for (var key in obj) res.push(key)
-    return res;
-};
-
-function toS (obj) { return Object.prototype.toString.call(obj) }
-function isDate (obj) { return toS(obj) === '[object Date]' }
-function isRegExp (obj) { return toS(obj) === '[object RegExp]' }
-function isError (obj) { return toS(obj) === '[object Error]' }
-function isBoolean (obj) { return toS(obj) === '[object Boolean]' }
-function isNumber (obj) { return toS(obj) === '[object Number]' }
-function isString (obj) { return toS(obj) === '[object String]' }
-
-var isArray = Array.isArray || function isArray (xs) {
-    return Object.prototype.toString.call(xs) === '[object Array]';
-};
-
-var forEach = function (xs, fn) {
-    if (xs.forEach) return xs.forEach(fn)
-    else for (var i = 0; i < xs.length; i++) {
-        fn(xs[i], i, xs);
-    }
-};
-
-forEach(objectKeys(Traverse.prototype), function (key) {
-    traverse[key] = function (obj) {
-        var args = [].slice.call(arguments, 1);
-        var t = new Traverse(obj);
-        return t[key].apply(t, args);
-    };
-});
-
-});
-
-require.define("/lib/bundle.js",function(require,module,exports,__dirname,__filename,process,global){var json = require('../configs/sample.config.json');
-var mouseAround = require('../lib/mouse.around');
-var traverse = require('traverse');
-var div = document.getElementById('json');
-var tenants = document.getElementById('tenants');
-var container = document.getElementById('container');
-var hoverbot = document.getElementById('hoverbot');
-var parent;
-var path = [];
-var current_keyup_listener;
-function init(obj, el){
-	traverse(obj).forEach(function(x){
-		if(this.isRoot) return;
-		if(el.classList.contains('array')){
-			if(this.level < 2){
-				var node = document.createElement('div');
-				node.classList.add('cursor')
-				node.textContent = (this.isLeaf ? this.node : type(obj[this.key]));
-	//			node.href="#" + this.key;
-				node._data = obj[this.key];
-				node._meta = this;
-				node._path = [];
-				node._node = x;
-				node.addEventListener('click', tenantOnClick, true)
-				mouseAround(node, function(evt, el, pos, start, end){
-					hoverbot.style.left = 50 + evt.clientX + "px";
-					hoverbot.style.top = evt.clientY - 25 + 'px';
-					if(end) hoverbot.style.left = '-1000px'; 
-				});
-				el.appendChild(node);
-			}
-			return
-		}
-		if(this.level < 2){			
-			var node = document.createElement('div');
-			node.classList.add('cursor')
-			
-			node.textContent = this.key + (this.isLeaf ? (' : ' + this.node) : '');
-//			node.href="#" + this.key;
-			node._data = obj[this.key];
-			node._meta = this;
-			node._path = [];
-			node._node = x;
-			node.addEventListener('click', tenantOnClick, true)
-			mouseAround(node, function(evt, el, pos, start, end){
-				hoverbot.style.left = 25 + evt.clientX + "px";
-				hoverbot.style.top = evt.clientY - 10 + 'px';
-				if(end) hoverbot.style.left = '-1000px'; 
-			});
-			el.appendChild(node);
-			return				
-		}
-	});
-	var menu = document.createElement('div');
-	menu.classList.add('menu');
-//	el.appendChild(menu)
-};
-
-init(json, tenants);
-
-function tenantOnClick(e){
-	var node = e.target;
-	node.setAttribute('contenteditable', 'true')
-	if(current_keyup_listener){
-		current_keyup_listener.removeEventListener('keyup', function(){}, true);
-		current_keyup_listener = node
+	xhr.send();
+	save.onclick = function(){
+		var json = editor.get();
+		var trx = new XMLHttpRequest();
+		trx.open('GET', '../setconfig?config=' + encodeURI(JSON.stringify(json)), true)
+		trx.send()
 	}
-	node.addEventListener('keyup', function(e){
-		if(node._meta.isLeaf) node._meta.parent.node[node._meta.key] = node.textContent;
-		console.log(node.textContent)
-	}, true);
-	sibling(node.parentNode);
-	var className = Array.isArray(node._data) ? 'array' : typeof node._data ;
-	var div = adiv(className);
-	container.appendChild(div);
-	node._path.push(div);
-	if(node._meta.isLeaf) return;
-	init(node._data, div);
-};
+    // save json
+    function save() {
+		
+		
+    }
+})
 
-function sibling(parent){
-	var sibs = Array.prototype.slice.call(parent.children);
-	if(sibs.length){
-		sibs.forEach(function(e){
-			followPath(e)
-		})		
-	}
-};
-
-function followPath(el){
-	if(el._path && el._path.length){
-		var node = el._path.pop();
-		sibling(node);
-		container.removeChild(node);
-	}
-};
-
-function adiv(c){
-	var node = document.createElement('div');
-	node.classList.add(c);
-	return node
-};
-
-function displayParams(node){
-	var arr = Array.prototype.slice.call(node.children);
-	arr.forEach(function(e){
-		node.removeChild(e)
-	});	
-};
-
-
-/*
-function init(){
-	traverse(json).forEach(function(x){
-		if(this.isRoot) return;
-		if(this.notLeaf) {
-			this.before(before);
-			this.post(after);
-			return
-		};
-		if(this.isLeaf){
-			var div = document.createElement('p');
-			div.classList.add('child');
-			div.textContent = this.key + ' : ' + x;
-			parent.appendChild(div)
-			return
-		}
-
-	})	
-}
-
-init();
-
-function before(x){
-	this._length = this.keys.length
-	level = this.level;
-
-	var node = document.createElement('div');
-	node.setAttribute('tabindex', ++tab)
-	var className = Array.isArray(x) ? 'array' : typeof x ;
-	var h1 = document.createElement('div');
-	h1.classList.add('key')
-	h1.text = h1.text || h1.textContent;
-	h1.textContent = this.key + ' : ' + type(className);
-	node.appendChild(h1)
-	node.classList.add(className);
-	parent.appendChild(node);
-	parent = node;
-	this.el = node;
-}
-
-function after(x){
-	if(--this._length == 0) {
-		parent = parent.parentNode
-	}
-}	
-*/
-
-function type(x){
-	var x = Array.isArray(x) ? 'array' : typeof x ;	
-	if(x == 'array') return '[ Array ]';
-	if(x == 'object') return '{ Object }';
-	if(x == 'function') return 'fn( ... )'
-}
 });
 require("/lib/bundle.js");
 })();
